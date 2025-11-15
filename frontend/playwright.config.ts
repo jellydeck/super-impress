988250195f0edcf75fa05fc7fc8b2d9ce1c69539
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2025-11-15 16:28:52 +0530

    more cleanup

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
-		cwd: './frontend',
 		port: 4173
 	},
-	testDir: './frontend/e2e'
+	testDir: 'e2e'
 });
